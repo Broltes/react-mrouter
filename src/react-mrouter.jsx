c287bf22ac48f905b79bcf8539ef09620d88abe2
Broltes <broltes@qq.com>
2016-07-07 17:06:53 +0800

    重写base view逻辑

--- src/react-mrouter.jsx
+++ src/react-mrouter.jsx
@@ -4,41 +4,48 @@ import './animation.less';
 
 var vp;// view params，视图通过路由传递的参数
 var lastTouch;
+function getPath(url) {
+    return (url || location.hash).replace(/^#\//, '');
+}
 
 export var Router = React.createClass({
     getInitialState: function() {
         return {
-            visuals: [],
+            visuals: []
         };
     },
     getDefaultProps: function () {
         return {
             transitionName: 'zoom-fade',
-            viewClass: 'view'
+            viewClass: 'view',
+            baseView: 'base'
         };
     },
 
     componentDidMount: function(){
         var that = this;
-        var viewClass = this.props.viewClass;
+        var viewClass = that.props.viewClass;
+        var baseView = that.props.baseView;
 
         window.addEventListener('click', function(){
             lastTouch = Date.now();
         }, true);
 
-        window.onpopstate = function(){
+        function onpopstate(){
             // 通用路由规则
             // /{viewName}/{params}
-            var path = location.hash.replace(/^#\//, '');
+            var path = getPath() || baseView;
             var actions = path.split('/');
             var viewName = actions.shift();
             var params = vp;
             vp = null;// reset
 
-            var visuals = that.state.visuals.slice();
-            var lastViewPath = visuals.length > 1 ? visuals.slice(-2)[0].props.path : '';
+            // 加载baseView时重置view队列
+            // 防止 初始View!=BaseView时，fix back 异常
+            var visuals = viewName == baseView ? [] : that.state.visuals.slice();
             var currentView = visuals.slice(-1)[0];
             var currentViewPath = currentView ? currentView.props.path : '';
+            var lastViewPath = visuals.length > 1 ? visuals.slice(-2)[0].props.path : '';
 
             // target router is current router
             if(path == currentViewPath) return;
@@ -58,7 +65,7 @@ export var Router = React.createClass({
                 if(View) {
                     // open new view
                     visuals.push(
-                        <div className={viewClass} key={visuals.length} path={path}>
+                        <div className={viewClass} key={'v' + visuals.length} path={path}>
                             <View actions={actions} params={params}/>
                         </div>
                     );
@@ -69,24 +76,22 @@ export var Router = React.createClass({
             }
 
             that.setState({visuals: visuals});
-        };
-
-        window.onpopstate();// start router
+        }
+        window.addEventListener('popstate', onpopstate);
+        onpopstate();// start router
     },
     render: function(){
-        var { views, transitionName, viewClass } = this.props;
-        var Base = views.base;
-
-        var showing = [].concat();
+        const { views, transitionName, viewClass } = this.props;
+        const { visuals } = this.state;
 
         return (
             <ReactCSSTransitionGroup
                 transitionName={transitionName}
                 transitionEnterTimeout={400}
                 transitionLeaveTimeout={400}
+                transitionEnter={visuals.length > 1}
                 component="div" id="views">
-                <div className={viewClass}><Base/></div>
-                {this.state.visuals}
+                {visuals}
             </ReactCSSTransitionGroup>
         );
     }
@@ -94,16 +99,18 @@ export var Router = React.createClass({
 
 // router api
 export default {
-    goto: function(actions, params){
+    goto(actions, params){
         vp = params;// set view params first
         location.hash = '#' + actions;// then router to view
     },
-    back: function(){
+    getPath,
+    back(){
         lastTouch = 0;// disable back fix
         if(location.hash.replace(/^#\//, '')) history.back();
     },
-    reload: function(){
+    reload(){
+        /* global plus */
         if(window.plus) plus.runtime.restart();
-        else location.href = location.origin;
-    },
+        else location.href = location.href.replace(location.hash, '');
+    }
 };
